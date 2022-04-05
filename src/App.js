0b70f7a6be54d24f5585cf159c0ba0e36e584db3
Liezl Mier <liez.dev@gmail.com>
2022-04-05 09:28:09 +0800

    Design and input values done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
 import "./styles.css";
-
+import React, { useState } from "react";
 export default function App() {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: ""
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues((prev) => {
+      return {
+        ...prev,
+        [name]: value
+      };
+    });
+  };
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="form-container">
+      <form className="register-form">
+        {/* Uncomment the next line to show the success message */}
+        {/* <div className="success-message">Success! Thank you for registering</div> */}
+        <input
+          id="first-name"
+          className="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+          value={values.firstName}
+          onChange={(e) => handleChange(e)}
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="first-name-error">Please enter a first name</span> */}
+        <input
+          id="last-name"
+          className="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+          value={values.lastName}
+          onChange={(e) => handleChange(e)}
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="last-name-error">Please enter a last name</span> */}
+        <input
+          id="email"
+          className="form-field"
+          type="text"
+          placeholder="Email"
+          name="email"
+          value={values.email}
+          onChange={(e) => handleChange(e)}
+        />
+        {/* Uncomment the next line to show the error message */}
+        {/* <span id="email-error">Please enter an email address</span> */}
+        <button className="form-field" type="submit">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
